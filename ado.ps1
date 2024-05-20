0e30e5c78866624ee83b2e3136daf29add70bf1a
discollizard <discollizard@github.com>
2024-05-20 02:33:37 -0300

    process can now be stopped via system tray

--- ado.ps1
+++ ado.ps1
@@ -9,12 +9,12 @@ Made by: discollizard
 ---
 TODO: make it able to read a file with key-value pairs indicating which extensions go where
 TODO: make it able to scan files by regex instead of extension
-TODO: make it function with other time magnitudes other than minutes
+TODO: make it function with other time magnitudes other than seconds
 ---
 
-usage: ado {.extension} {.source-folder} {.destination-folder} {interval-minutes}
+usage: ado {.extension} {.source-folder} {.destination-folder}
 
-example: ado .txt c:\users\amelia\desktop c:\users\amelia\documents\my_writings 10
+example: ado .txt c:\users\amelia\desktop c:\users\amelia\documents\my_writings
 
 #>
 
@@ -22,31 +22,26 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 $notifyIcon = New-Object System.Windows.Forms.NotifyIcon
-$notifyIcon.Icon = [System.Drawing.SystemIcons]::Application
+$notifyIcon.Icon = [System.Drawing.SystemIcons]::WinLogo
 $notifyIcon.Text = "Auto folder organizer"
+$notifyIcon.Visible = $true
 
 $contextMenu = New-Object System.Windows.Forms.ContextMenu
 $menuExit = New-Object System.Windows.Forms.MenuItem "Stop process"
 
 $contextMenu.MenuItems.Add($menuExit)
 
-$notifyIcon.ContextMenu = $contextMenu
-
-$notifyIcon.Visible = $true
-
-$menuExit.add_Click({
+$menuExit.add_Click({    
     $notifyIcon.Visible = $false
     $notifyIcon.Dispose()
     [System.Windows.Forms.Application]::Exit()
 })
 
+$notifyIcon.ContextMenu = $contextMenu
 $username = $env:USERNAME 
 
-while($True){
-    [ System.Windows.Forms.Application ]::DoEvents()
+while($notifyIcon.Visible){
+    [System.Windows.Forms.Application]::DoEvents()
     Get-ChildItem -Path $($args[1]) -Filter "*$($args[0])" | Move-Item -Destination $($args[2]) -Force
-    Start-Sleep -Seconds $($args[3] * 60)
+    Start-Sleep -Seconds 1
 }
-
-
-
