e2fe5ff44777039349751e2ce6de1e8f3705192f
discollizard <discollizard@github.com>
2024-05-21 02:46:21 -0300

    new icon added + settings started + debug mode started + new ideas

--- ado.ps1
+++ ado.ps1
@@ -8,8 +8,12 @@ Made by: discollizard
 
 ---
 TODO: make it able to read a file with key-value pairs indicating which extensions go where
+    SUB-TODO: make it startable by both command line and persistent GUI settings
 TODO: make it able to scan files by regex instead of extension
-TODO: make it function with other time magnitudes other than seconds
+TODO: make a GUI for it 
+    SUB-TODO: make it a desktop icon
+TODO: make automated testing for it
+TODO: add debug flag to increase output verbosity
 ---
 
 usage: ado {.extension} {.source-folder} {.destination-folder}
@@ -21,8 +25,33 @@ example: ado .txt c:\users\amelia\desktop c:\users\amelia\documents\my_writings
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+# $debugMode = $false
+$debugMode = $true
+
+
+# $transactions = if($(Get-Content .\settings.json))
+$transactions = if($false)
+ { Get-Content .\settings.json -Raw | ConvertFrom-Json }
+ else 
+ {
+    Write-Host $args
+    if ($args.Count -eq 3) 
+        { 
+            @(
+                @{
+                    "source" = $args[1];
+                    "destination" = $args[2];
+                    "term" = $args[0];
+                }
+            ) 
+        }
+    else 
+        { throw "No settings.json file found nor enough arguments for a command line use `n example: ado {.extension} {.source-folder} {.destination-folder}" }
+    }
+
+
 $notifyIcon = New-Object System.Windows.Forms.NotifyIcon
-$notifyIcon.Icon = [System.Drawing.SystemIcons]::WinLogo
+$notifyIcon.Icon = New-Object System.Drawing.Icon("./icon.ico")
 $notifyIcon.Text = "Auto folder organizer"
 $notifyIcon.Visible = $true
 
@@ -42,6 +71,24 @@ $username = $env:USERNAME
 
 while($notifyIcon.Visible){
     [System.Windows.Forms.Application]::DoEvents()
-    Get-ChildItem -Path $($args[1]) -Filter "*$($args[0])" | Move-Item -Destination $($args[2]) -Force
-    Start-Sleep -Seconds 1
+
+    foreach($transaction in $transactions){
+        try {
+            $result = Get-ChildItem -ErrorAction Stop -Path $($transaction["source"]) -Filter "*$($transaction["term"])" | Move-Item -Destination $($transaction["destination"]) -Force
+            Write-Output $result
+            if($debugMode){
+                if($result){
+                    Write-Output "Files transfered!: $($transaction["source"]), extension searched: $($transaction["term"])"
+                } else {
+                    throw [System.Management.Automation.ItemNotFoundException]::new()
+                } 
+            }
+        }  catch [System.Management.Automation.ItemNotFoundException] {
+            if($debugMode) {
+                Write-Output "Files not found: $($transaction["source"]), extension searched: $($transaction["term"])"
+            }
+        }
+    }
+
+    Start-Sleep -Seconds 3
 }
